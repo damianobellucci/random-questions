dfaf8c864ebe0fb70baa6ff09114114d1a18e29e
Damiano <belluccidamiano@gmail.com>
2021-01-24 18:12:23 +0100

    added frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 
 import express from "express"
+import cors from "cors"
 import getQuestion from "./routers/getQuestion.js"
 const app = express()
+
+app.use(cors())
 
 app.use("/getQuestion", getQuestion)
 app.listen(5000, console.log("listening on port 5000"))
