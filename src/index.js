b9fd967141b9ecfcfc00bed37698135a14da4904
Damiano <belluccidamiano@gmail.com>
2021-01-24 21:10:17 +0100

    configured for host

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-
 import express from "express"
 import cors from "cors"
 import getQuestion from "./routers/getQuestion.js"
 const app = express()
-
 app.use(cors())
 
 app.use("/getQuestion", getQuestion)
-app.listen(5000, console.log("listening on port 5000"))
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 5000;
+}
+app.listen(process.env.PORT, console.log("listening on port 5000"))
